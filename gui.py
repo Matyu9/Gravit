a636bf21d1c1265dcc52918f1dd3b55f20e3b74d
anat <49984698+anat3000@users.noreply.github.com>
2019-12-03 13:28:48 +0100

    Update gui.py
    Now the code works !
    WARNING : this is a test for rendering moving circle, not the final Gravit.

--- gui.py
+++ gui.py
@@ -12,16 +12,18 @@ def rcolor():
 def gen(gravit):
     pass
 
-def main(FPS):
+def main(FPS=60, win_size=(640, 480)):
     pygame.init()
 
-    spacetime = pygame.display.set_mode((640, 480))
+    spacetime = pygame.display.set_mode(win_size)
 
     fps_lim = pygame.time.Clock()
+    pygame.key.set_repeat(400, 30)
+
     color = rcolor()
     obj1 = pygame.draw.circle(spacetime, color, (320, 240), 50)
-    
-    fond = pygame.image.load("background.jpg").convert()
+
+    fond = pygame.Surface(win_size).convert()
 
     game_continue = True
     while game_continue:
@@ -29,13 +31,22 @@ def main(FPS):
             if event.type == QUIT:
                 game_continue = False
 
-        obj1.move_ip(3, 0)
+            if event.type == KEYDOWN:
+                if event.key == K_RIGHT:
+                    obj1.move_ip(2, 0)
+                if event.key == K_LEFT:
+                    obj1.move_ip(-2, 0)
+                if event.key == K_DOWN:
+                    obj1.move_ip(0 ,2)
+                if event.key == K_UP:
+                    obj1.move_ip(0, -2)
+
         spacetime.blit(fond, (0, 0))
 
-        obj1 = pygame.draw.circle(spacetime, color, (obj1.x+50, obj1.y+50), 50)
+        obj1 = pygame.draw.circle(spacetime, color, obj1.center, 50)
 
-        pygame.display.update()
+        pygame.display.flip()
         fps_lim.tick(FPS)
 
-main(10)
+main(60)
 exit(0)
