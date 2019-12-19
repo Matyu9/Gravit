24929ef2323acf0ee89f1651935bb3af032d8692
anat <49984698+anat3000@users.noreply.github.com>
2019-12-19 16:40:16 +0100

    Update log.py
    The src now isn't standalone

--- gravit/log.py
+++ gravit/log.py
@@ -7,12 +7,3 @@ class Log:
     def new_log(self, log):
         datetime = time.strftime("[%d/%m/%Y | %H:%M:%S] ", time.localtime(time.time()))
         self.log += "\n"+datetime+log
-
-l = Log("Test log v1.0")
-print(l.log)
-l.new_log("The bacon is with the eggs !")
-print(l.log)
-time.sleep(5)
-l.new_log("The bacon is with the eggs !")
-print(l.log)
-
