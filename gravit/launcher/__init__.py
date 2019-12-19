2286889064684ea020cb89e306be24b9ffc752a9
anat <49984698+anat3000@users.noreply.github.com>
2019-12-19 16:36:37 +0100

    Update __init__.py
    Now the script isn't standalone

--- gravit/launcher/__init__.py
+++ gravit/launcher/__init__.py
@@ -16,7 +16,7 @@ class Launcher(tk.Tk):
     def ghpage():
     	wb.new("https://github.com/anat3000/Gravit")
 
-    def __init__(self, sim_to_launch=None):
+    def __init__(self, sim_to_launch):
         #init of the super-class
         super().__init__()
 
@@ -24,15 +24,10 @@ class Launcher(tk.Tk):
         self.geometry("960x720")
 
         #set the simulation which will launched (it's a launcher, no ?')
-        self.sim_to_launch = sim_to_launch
+        self.simulation = sim_to_launch
         #menu bar
         self.menubar = MenuBar(self)
         self.config(menu=self.menubar)
         # play button
         self.play_button = tk.Button(self, text="Start the simulation", height=5, command=self.askplay)
         self.play_button.pack(side="bottom", fill="x")
-
-
-l = Launcher()
-
-l.mainloop()
