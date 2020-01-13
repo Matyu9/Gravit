b2d6dc3166ee9f4c9fe47b4116a1aa862485baa9
anatom <49984698+anatom3000@users.noreply.github.com>
2020-01-13 12:15:15 +0100

    Delete bodygroup.py
    Can be replaced py a list

--- gravit/simulation/calculator/bodygroup.py
+++
@@ -1,9 +0,0 @@
-from gravit.simulation.calculator.utility import *
-
-class BodyGroup:
-	
-    def __init__(self, *bodies):
-        self.bodies = bodies
-        
-        for body in self.bodies:
-            body.master = self
