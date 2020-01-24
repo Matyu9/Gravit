362fb1aaaee41f29e84a864e3e6e16ec713c1f20
anatom <49984698+anatom3000@users.noreply.github.com>
2020-01-24 12:12:43 +0100

    Update utility.py
    Fixed bugs.

--- gravit/simulation/calculator/utility.py
+++ gravit/simulation/calculator/utility.py
@@ -2,14 +2,15 @@ from math import sqrt, atan
 
 class Vector(tuple):
 
-    def __init__(self, x, y):
-        super().__init__((x, y))
+    def __new__(cls, x, y):
+        self = super().__new__(cls, (x, y))
+        return self
 
     def abs(self):
-        return sqrt(self.x**2+self.y**2)
+        return sqrt(self[0]**2+self[1]**2)
 
     def dir(self):
-        return atan(x/y)
+        return atan(self[0]/self[1])
 
     def __add__(self, other):
         return Vector(self[0]+other[0], self[1]+other[1])
@@ -17,12 +18,36 @@ class Vector(tuple):
     def __mult__(self, nb):
         return Vector(self[0]*nb, self[1]*nb)
 
+    def __repr__(self):
+        return "Vector("+str(self[0])+", "+str(self[1])+")"
+
 class Position(tuple):
-    def __init__(self, x, y):
-        super().__init__((x, y))
+
+    def __new__(cls, x, y):
+        self = super().__new__(cls, (x, y))
+        return self
 
     def __add__(self, other):
         return Position(self[0]+other[0],
                         self[1]+other[1])
+
+    def __rtruediv__(self, other):
+        return Position(self[0]+other[0],
+                        self[1]+other[1])
+
     def to(self, other):
         return Vector(other[0]-self[0], other[1]-self[1])
+
+    def __repr__(self):
+        return "Position("+str(self[0])+", "+str(self[1])+")"
+
+class BodyList(list):
+    def __init__(self, *args):
+        super().__init__(args)
+
+    def __sub__(self, other):
+        n = BodyList()
+        for i in self:
+            if not i in other:
+                n.append(i)
+        return n
