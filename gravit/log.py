792a685d4d4d7e143bd6f4a904b7c61455575f14
anatom <49984698+anatom3000@users.noreply.github.com>
2020-09-10 12:40:51 +0200

    Delete log.py
    No longer used...

--- gravit/log.py
+++
@@ -1,9 +0,0 @@
-import time
-
-class Log:
-    def __init__(self, log=""):
-        self.log = log
-
-    def new_log(self, log):
-        datetime = time.strftime("[%d/%m/%Y | %H:%M:%S] ", time.localtime(time.time()))
-        self.log += "\n"+datetime+log
